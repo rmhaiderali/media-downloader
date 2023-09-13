b4ba6198826d0168da40a8136ae821d4d2a1fe10
Haider Ali <rajahaider745@gmail.com>
2023-09-13 13:42:06 +0500

    Update proxy.php

--- backend/proxy.php
+++ backend/proxy.php
@@ -3,7 +3,7 @@ function send_error($code, $message)
 {
     http_response_code($code);
     header("Content-Type: application/json");
-    echo json_encode(["error" => $message]);
+    echo json_encode(["error" => $message], JSON_UNESCAPED_SLASHES);
     exit;
 }
 
