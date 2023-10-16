6f12f1c35ce9408b38fc91f15f334eaf020ad4dd
Haider Ali <rajahaider745@gmail.com>
2023-10-16 23:39:52 +0500

    prettify

--- frontend/current/src/ddm/themeColors.js
+++ frontend/current/src/ddm/themeColors.js
@@ -1,62 +1,62 @@
 String.prototype.format = function () {
   return this.replace(/{(\d+)}/g, (match, number) => {
-    return typeof arguments[number] !== "undefined" ? arguments[number] : match;
-  });
-};
+    return typeof arguments[number] !== "undefined" ? arguments[number] : match
+  })
+}
 
 const parseColor = (string) => {
-  const rgbRegex = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/;
-  const rgbaRegex = /^rgba\((\d+),\s*(\d+),\s*(\d+),\s*([01]?(\.\d+)?)\)$/;
+  const rgbRegex = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/
+  const rgbaRegex = /^rgba\((\d+),\s*(\d+),\s*(\d+),\s*([01]?(\.\d+)?)\)$/
 
-  const rgbMatch = string.match(rgbRegex);
-  const rgbaMatch = string.match(rgbaRegex);
+  const rgbMatch = string.match(rgbRegex)
+  const rgbaMatch = string.match(rgbaRegex)
 
   if (rgbaMatch) {
     return {
       r: parseInt(rgbaMatch[1]),
       g: parseInt(rgbaMatch[2]),
       b: parseInt(rgbaMatch[3]),
-      a: parseFloat(rgbaMatch[4]),
-    };
+      a: parseFloat(rgbaMatch[4])
+    }
   } else if (rgbMatch) {
     return {
       r: parseInt(rgbMatch[1]),
       g: parseInt(rgbMatch[2]),
-      b: parseInt(rgbMatch[3]),
-    };
-  } else return this;
-};
+      b: parseInt(rgbMatch[3])
+    }
+  } else return this
+}
 
 const colorMixCSS = (finalColorRGB, t) => {
   return CSS.supports("color", "color-mix(in srgb, AccentColor, transparent)")
     ? "color-mix(in srgb, var(--theme), transparent " + t + "%)"
-    : "rgba({0}, 0.{1})".format(finalColorRGB, 100 - t);
-};
+    : "rgba({0}, 0.{1})".format(finalColorRGB, 100 - t)
+}
 
 const colorKeywordToRGB = (colorKeyword) => {
-  const el = document.createElement("div");
-  el.style.color = colorKeyword;
-  document.documentElement.appendChild(el);
-  const value = window.getComputedStyle(el).color;
-  document.documentElement.removeChild(el);
-  return value;
-};
+  const el = document.createElement("div")
+  el.style.color = colorKeyword
+  document.documentElement.appendChild(el)
+  const value = window.getComputedStyle(el).color
+  document.documentElement.removeChild(el)
+  return value
+}
 
-const AccentColor = parseColor(colorKeywordToRGB("AccentColor"));
+const AccentColor = parseColor(colorKeywordToRGB("AccentColor"))
 
 const isAccentColorProvided = Boolean(
   AccentColor.r || AccentColor.g || AccentColor.b
-);
+)
 
 const finalColorRGB = isAccentColorProvided
   ? "{0}, {1}, {2}".format(AccentColor.r, AccentColor.g, AccentColor.b)
-  : "220, 53, 69";
+  : "220, 53, 69"
 
 document.body.style.setProperty(
   "--theme",
   isAccentColorProvided ? "AccentColor" : "#dc3545"
-);
+)
 
-document.body.style.setProperty("--theme-t50", colorMixCSS(finalColorRGB, 50));
+document.body.style.setProperty("--theme-t50", colorMixCSS(finalColorRGB, 50))
 
-document.body.style.setProperty("--theme-t80", colorMixCSS(finalColorRGB, 80));
+document.body.style.setProperty("--theme-t80", colorMixCSS(finalColorRGB, 80))
