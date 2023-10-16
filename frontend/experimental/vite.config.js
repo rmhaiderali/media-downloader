50de228097ec0073026f6a98db3e504732b4462e
Haider Ali <rajahaider745@gmail.com>
2023-10-17 00:18:55 +0500

    update build

--- frontend/experimental/vite.config.js
+++ frontend/experimental/vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react"
 const stringer = (target) =>
   typeof target === "string" ? "\"" + target + "\"" : target
 
-const server = ["/", "http://localhost:3001/", "http://159.223.36.123:3001/"][0]
 const proxy = [false, "https://ueso.000webhostapp.com/proxy.php"][0]
+const server = ["/", "http://localhost:3001/", "http://159.223.36.123:3001/"][0]
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/experimental/",
   plugins: [react()],
   build: { target: "es6" },
-  define: { SERVER: stringer(server), PROXY: stringer(proxy) }
+  define: { PROXY: stringer(proxy), SERVER: stringer(server) }
 })
